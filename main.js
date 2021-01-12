eb2b349c910ec506d47b7cb7a54ecd78711eb205
yw <ywang035@gmail.com>
2021-01-12 14:22:06 +1100

    2nd-edition
    new design and style (desktop site only)

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-const jokeBtn = document.querySelector('#joke-btn');
-const jokeDiplay = document.querySelector('#joke-display');
+// function navbarChanger(){
+//   let sections = document.querySelectorAll('section');
+//   for (let i = 0; i < sections.length; i++){
+//    console.log(sections[i]);
+//   }
+// }
+// navbarChanger();
 
-jokeBtn.addEventListener('click', getJoke);
 
-function getJoke(){
-    fetch('https://official-joke-api.appspot.com/random_joke')
-    // .then(response => response.json())
-    // .then(json => console.log(json));
-    .then(response => {
-        if (!response.ok) {
-            jokeDiplay.innerHTML = "Something went wrong. Try again later!";
-        }else {
-            return response.json();
+
+const quote = document.querySelector('#quote');
+const author = document.querySelector('#author');
+
+function get_quote_of_the_day() {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            // Access the result here
+            //  alert(this.responseText);
+
+            var obj = JSON.parse(this.responseText);
+            // alert(obj.contents.quotes[0].quote);
+            quote.innerHTML = "“ " + obj.contents.quotes[0].quote + " ”";
+            author.innerHTML = "---- " + obj.contents.quotes[0].author;
+            document.body.style.display = 'block';
+        } else {
+            //  alert("failed");
         }
-    })
-    .then(json => jokeDiplay.innerHTML = `${json.setup} ${json.punchline}`);
+
+    };
+    xhttp.open("GET", `${proxy}https://quotes.rest/qod?category=inspire`, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
 }
-
-
+get_quote_of_the_day()
