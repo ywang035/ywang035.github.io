6aa7a0b46ffd8b43272634f42fd3216a62438b37
yw <ywang035@gmail.com>
2021-01-30 13:46:41 +1100

    4th commit
    add indicator to nav item

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-// function navbarChanger(){
-//   let sections = document.querySelectorAll('section');
-//   for (let i = 0; i < sections.length; i++){
-//    console.log(sections[i]);
-//   }
-// }
-// navbarChanger();
+// change nav item style when scrolling through sections
+const navs = document.querySelectorAll('li');
+var navArr = Array.from(navs);
+
+var screenHeight = window.innerHeight;
+window.addEventListener("resize", function(){
+    screenHeight = window.innerHeight
+});
+
+document.addEventListener('scroll', function(e){
+    // console.log(document.documentElement.scrollTop);
+    let topPosition = document.documentElement.scrollTop;
+    let bottomPosition = topPosition + screenHeight;
+    if(topPosition >= screenHeight && topPosition < screenHeight*2){
+        navArr[0].classList.add('nav-active');
+        navArr[1].classList.remove('nav-active');
+    }else if(topPosition >= screenHeight*2){
+        navArr[0].classList.remove('nav-active');
+        navArr[1].classList.add('nav-active');
+    }else{
+        navArr[0].classList.remove('nav-active');
+        navArr[1].classList.remove('nav-active');
+    }
+});
 
 
 
+// get daily quote from API
 const quote = document.querySelector('#quote');
 const author = document.querySelector('#author');
 
@@ -15,7 +33,6 @@ function get_quote_of_the_day() {
     const proxy = "https://cors-anywhere.herokuapp.com/";
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-
         if(this.readyState == 4){
             if(this.status == 200){
                 var obj = JSON.parse(this.responseText);
@@ -28,20 +45,6 @@ function get_quote_of_the_day() {
                 document.body.style.display = 'block';
             }
         }
-
-
-        // if (this.readyState == 4 && this.status == 200) {
-        //     // Access the result here
-        //     //  alert(this.responseText);
-
-        //     var obj = JSON.parse(this.responseText);
-        //     quote.innerHTML = "“ " + obj.contents.quotes[0].quote + " ”";
-        //     author.innerHTML = "---- " + obj.contents.quotes[0].author;
-        //     // document.body.style.display = 'block';
-        // } else {
-        //     //  alert("failed");
-        // }
-
     };
     xhttp.open("GET", `${proxy}https://quotes.rest/qod?category=inspire`, true);
     xhttp.setRequestHeader("Content-type", "application/json");
